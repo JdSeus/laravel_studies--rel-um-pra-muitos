80d708c185eb2719085cd3ad19cf8a484b4993e9
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-20 09:17:54 -0300

    utilizando o hasmany

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,28 @@ Route::get('/produtos', function () {
         echo "<table>";
     }
 });
+
+Route::get('/categoriasprodutos', function () {
+    $cats = Categoria::all();
+    if (count($cats) === 0)
+    {
+        echo "<h4>Você não possui nenhuma categoria cadastrada.</h4>";
+    }
+    else
+    {
+        foreach($cats as $c) {
+            echo "<p>" . $c->id . " - " . $c->nome . "</p>";
+            $produtos = $c->produtos;
+
+            if (count($produtos) > 0) {
+                echo "<ul>";
+                foreach($produtos as $p) {
+                    echo "<li>";
+                    echo $p->nome;
+                    echo "</li>";
+                }
+                echo "</ul>";
+            }
+        }
+    }
+});
