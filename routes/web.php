892cc491acf62f9a328e70951b5fe654e3949084
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-20 09:31:35 -0300

    associacoes atraves de relacionamentos

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,29 @@ Route::get('/categoriasprodutos/json', function () {
     $cats = Categoria::with('produtos')->get();
     return $cats->toJson();
 });
+
+Route::get('/adicionarproduto', function () {
+    $cat = Categoria::find(1);
+
+    $p = new Produto();
+    $p->nome = "Meu novo Produto";
+    $p->estoque = 10;
+    $p->preco = 100;
+    $p->categoria()->associate($cat);
+
+    $p->save();
+
+    return $p->toJson();
+});
+
+Route::get('/removerprodutocategoria', function () {
+    $p = Produto::find(10);
+    if (isset($p)) {
+        $p->categoria()->dissociate();
+        $p->save();
+        return $p->toJson();
+    }
+
+    return '';
+    
+});
